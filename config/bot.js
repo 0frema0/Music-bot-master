8287255b4328bd60d8770f7e185d94c9b84f8eab
frema2005 <90795827+frema2005@users.noreply.github.com>
2021-09-15 20:50:46 +0200

    ddf

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODU4NDUzNDUxNjcwODE0NzYx.YNeXFA.5aghyY5KLAyvyCL3cq723ZRBDuE',
+        prefix: '!',
+        activity: '**Playing** Music',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
